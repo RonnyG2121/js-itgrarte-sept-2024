ad635bf9d9530e3b1995f225eb82d3cc22a9e7a1
Ronny Omnier Gonzalez Vargas <yordemblo@gmail.com>
2024-09-07 17:00:34 +0200

    Agregado el código de la primera sección

---
+++ codigos/tema1/variable_constantes.js
@@ -0,0 +1,20 @@
+// Esto es un comentario de línea
+
+/*
+Esto es un comentario
+multilínea
+  */
+
+// Declarando variables
+let mi_variable = 10;
+
+let _nombre = "Ronny";
+let $dinero = 100.00;
+
+// Imprimiendo un mensaje en pantalla:
+console.log("Hola, mundo");
+console.log(mi_variable, _nombre, $dinero);
+
+// Declarando una constantte
+const PI = 3.14;
+console.log(PI);
