58ee81ba09902b4d2881f748ef9c7d7108f99330
Ronny Omnier Gonzalez Vargas <yordemblo@gmail.com>
2024-09-14 16:37:49 +0200

    Añadido el código de la clase 2

---
+++ codigos/tema2/tipo_datos_operadores.js
@@ -0,0 +1,62 @@
+// Trabajando  con tipos de datos y operadores
+
+let numero = 50;
+let decimal = 4.5;
+
+// console.log(typeof decimal);
+
+// Viendo tipo de datos string 
+let cadena = "Hola, saludos estudiantes buenos";
+// console.log(typeof cadena);
+
+// Mostrando el tipo de dato infinity
+let sin_valor;
+// console.log(sin_valor);
+
+// Tipo booleano
+let desicion = true;
+// console.log(typeof desicion);
+
+// Tipos de datos compuestos
+
+// Declarar un arreglo
+let arreglo = [50, "hola", true, [], {}];
+// console.log(arreglo);
+
+// Declarando objetos
+let mi_objeto = {
+    nombre: "Marcela",
+    edad: 25,
+    estudios: true
+}
+
+// console.log(mi_objeto);
+
+
+// Viendo los operadores
+// Operador de suma
+let num1= 15;
+let num2 = 10;
+let resultado_suma = num1 + num2;
+// console.log(resultado_suma);
+
+// Operador de resta
+let resultado_resta = num1 - num2;
+// console.log(resultado_resta);
+
+// Viendo el operador multiplicación
+let resultado_multi = num1 * num2;
+// console.log(resultado_multi);
+
+// Operador de división 
+let resultado_div = num1/ num2;
+// console.log(resultado_div);
+
+// Operador del módulo
+let resultado_modulo = num1 % num2;
+// console.log(resultado_modulo);
+
+// Operador de potencia
+let resultado_potencia = num1 ** num2;
+// console.log(resultado_potencia);
+
